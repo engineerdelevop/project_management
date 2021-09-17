8048b2e07e80a5171f5659e616636c54c06e6d79
Andres <31396408+andresmtr@users.noreply.github.com>
2021-09-16 19:57:39 -0500

    Login y logout finalizado

--- base/urls.py
+++ base/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import url
 # from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import login,logout
+from django.contrib.auth.decorators import login_required
 from base import views
-from .views import Inicio, Login
+from .views import Inicio, Login, logoutUsuario
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('', Inicio.as_view(), name='home')
-    path('home/',views.Inicio.index, name="home"),
-    path('',Login.as_view(), name='login'),
+    path('',login_required(Inicio.index), name="home"),
+    path('accounts/login/',Login.as_view(), name='login'),
+    path('logout/', login_required(logoutUsuario) , name ='logout')
 ]
--- base/views.py
+++ base/views.py
@@ -9,8 +9,8 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
 from django.views.generic.edit import FormView
-from django.contrib.auth import login
-from .formsLogin import FormularioLogin
+from django.contrib.auth import login, logout
+from .forms import FormularioLogin
 
 # 
 from django.views.generic import ListView
@@ -28,6 +28,7 @@ from django.db.models import Sum, Max, Min, Avg
 # Create your views here.
 
 
+# Se crea clase para el inicio de sesión
 class Login(FormView):
     template_name = 'login.html'
     form_class = FormularioLogin
@@ -47,53 +48,43 @@ class Login(FormView):
         login(self.request, form.get_user())
         return super(Login, self).form_valid(form)
 
+# se crea función para el logout, automaticamente se redirige al login
+def logoutUsuario(request):
+    logout(request)
+    return HttpResponseRedirect('/accounts/login/')
+
 
 class Inicio(ListView):
 
     def index(request):
 
         # contar No de proyectos
-        No_proyectos = Proyecto.objects.filter().count()
-        # return HttpResponse("El numero de proyectos es: " + str(No_proyectos))
+        # No_proyectos = Proyecto.objects.filter().count()
 
         # dataframe, don include the join tables
 
-        df = pd.DataFrame(list(Proyecto.objects.all().values()))
-
-        # inner join
-        # queryset = Proyecto.objects.select_related("estado").annotate(total_presupuesto=Count('estado')).order_by('-estado')
-        # queryset = Proyecto.objects.select_related("estado").annotate(total_presupuesto=Count('estado')).order_by('-estado')
-        
-        
+        # df = pd.DataFrame(list(Proyecto.objects.all().values()))
+            
         # select one part of the database
 
-        labels = []
-        data = []
-        # queryset4 = Proyecto.objects.values('estado__nombre').annotate(total_presupuesto=Sum('presupuesto')).order_by('-estado')
-        queryset5 = Proyecto.objects.values('estado__nombre').annotate(No_estado=Count('presupuesto')).order_by('-estado')
+        #labels = []
+        #data = []
+        #queryset5 = Proyecto.objects.values('estado__nombre').annotate(No_estado=Count('presupuesto')).order_by('-estado')
 
         # SQL conection
-        # SQL1 = Proyecto.objects.raw('SELECT * FROM base_proyecto')
-        SQL2 = Proyecto.objects.raw("SELECT 1 as id, base_proyecto.estado_id, base_estado.nombre, COUNT( base_estado.nombre) AS ['total'] FROM base_proyecto INNER JOIN base_estado ON base_estado.id = base_proyecto.estado_id GROUP BY base_proyecto.estado_id")
+        
+        #SQL2 = Proyecto.objects.raw("SELECT 1 as id, base_proyecto.estado_id, base_estado.nombre, COUNT( base_estado.nombre) AS ['total'] FROM base_proyecto INNER JOIN base_estado ON base_estado.id = base_proyecto.estado_id GROUP BY base_proyecto.estado_id")
 
-        df2 = pd.DataFrame(list(Proyecto.objects.raw("SELECT 1 as id, base_proyecto.estado_id, base_estado.nombre, COUNT( base_estado.nombre) AS ['Total'] FROM base_proyecto INNER JOIN base_estado ON base_estado.id = base_proyecto.estado_id GROUP BY base_proyecto.estado_id")))
+        #df2 = pd.DataFrame(list(Proyecto.objects.raw("SELECT 1 as id, base_proyecto.estado_id, base_estado.nombre, COUNT( base_estado.nombre) AS ['Total'] FROM base_proyecto INNER JOIN base_estado ON base_estado.id = base_proyecto.estado_id GROUP BY base_proyecto.estado_id")))
 
-        for i in SQL2:
-            labels.append(i.nombre)
-            print(i)
-            
-
-
+        #for i in SQL2:
+        #    labels.append(i.nombre)
+        #    print(i)
         
         context = {
-            'No_proyectos': No_proyectos,
-            'Resultado':queryset5,
+            #'No_proyectos': No_proyectos,
+            #'Resultado':queryset5,
         }
 
-        
         # print(labels)
-
-        
-        
-
         return render(request,'home.html',context)
